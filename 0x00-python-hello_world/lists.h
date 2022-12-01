c125de2e552760391f478a8033a6cb7d227185ea
MoniqueVaat <Vaatyoughm@gmail.com>
2022-12-01 02:19:14 -0800

    Run inline

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,7 +1,7 @@
-'# 'ifndef LISTS_H
-'# 'define LISTS_H
+#ifndef LISTS_H
+#define LISTS_H
 
-'# 'include <stdlib.h>
+#include <stdlib.h>
 
 
 typedef struct listint_s
@@ -11,7 +11,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t * add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
