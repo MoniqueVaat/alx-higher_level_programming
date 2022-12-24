69c089796efc63d49c482ad02a87401ea55a3921
MoniqueVaat <Vaatyoughm@gmail.com>
2022-12-23 16:42:18 -0800

    Integers division with debug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-def safe_print_list(mylist=[], x=0):
-    """Print x elememts of a list.
-
-    Args:
+def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x elements of a list that are integers.
+    
+	Args:
         my_list (list): The list to print elements from.
         x (int): The number of elements of my_list to print.
 
@@ -11,11 +11,11 @@ def safe_print_list(mylist=[], x=0):
         The number of elements printed.
     """
     ret = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             ret += 1
-        except IndexError:
-            break
+        except (ValueError, TypeError):
+            continue
     print("")
     return (ret)
